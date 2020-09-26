c1cc80fbd35a6e8a26f9addb407c46a3bb30ce7b
xTraco <67993552+brandonprogamer02@users.noreply.github.com>
2020-09-26 13:22:45 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.querySelector('#calculadora-dofus-touch').addEventListener('click', () => open('proyecto_app_calculador_danos_dofus_touch/index.html'));
+document.querySelector('#practica-crud-html-css-js').addEventListener('click', () => open('CRUD JS Local Storage/index.html'));
+document.querySelector('#aplicacion-maestros-minerd').addEventListener('click', () => open('proyecto1_duncar_amadis/index.html'));
