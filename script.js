df0218a54fd7e53b5fda761fbcc30085e1160522
brandon <brandonprogamer02@gmail.com>
2021-07-29 16:15:53 -0400

    Se aagrego el proyecto de master-chef-maquetacion y se hostio la aplicacion de notas basicas y la calculadora de dofus touch

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-document.querySelector('#calculadora-dofus-touch').addEventListener('click', () => open('proyecto_app_calculador_danos_dofus_touch/index.html'));
-document.querySelector('#practica-crud-html-css-js').addEventListener('click', () => open('notes-project/index.html'));
+document.querySelector('#calculadora-dofus-touch').addEventListener('click', () => open('https://dofus-touch-damage-calculator.netlify.app/'));
+document.querySelector('#practica-crud-html-css-js').addEventListener('click', () => open('https://basic-notes-app.netlify.app/'));
 document.querySelector('#aplicacion-maestros-minerd').addEventListener('click', () => open('https://minerd-teacher-app.netlify.app/'));
 document.querySelector('#quizzes-app').addEventListener('click', () => open('https://brandox-quizz-app.netlify.app/'));
 document.querySelector('#gesty-app').addEventListener('click', () => open('https://gesty-app.netlify.app/'));
 document.querySelector('#layouts-responsive').addEventListener('click', () => open('https://layouts-responsive.netlify.app/'));
 document.querySelector('#generate-stadistic-table-google-form').addEventListener('click', () => open('https://generate-stadistic-tables.herokuapp.com/'));
 document.querySelector('#chat-app').addEventListener('click', () => open('https://brandox-chat-app-frontend.herokuapp.com/'));
+document.querySelector('#mater-chef-maquetacion').addEventListener('click', () => open('https://master-chef-maquetacion.netlify.app/'));
+
